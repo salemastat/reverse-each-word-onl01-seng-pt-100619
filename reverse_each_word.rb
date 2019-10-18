3868a4e1087308fd220615943956795af4702dc5
Salem Asrat <salemasrat@gmail.com>
2019-10-18 14:58:08 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+def reverse_each_word(string)
+original_array = string.split("")
+return_array = []
+  original_array.each do |string| 
+    original_array << string.reverse 
+  end 
+  return_array.join("")
+  
+  
+def reverse_each_word(string)
+  array = string.split(" ") 
+  new_array = []
+  array.collect do|string| 
+    new_array << string.reverse 
+  end
+  new_array.join(" ")
+end
+    
